e63eb3cbf6761bc50ebf65c1a04f1283aa6391cf
Dale French <dalef@fmi.co.za>
2020-01-22 13:40:37 +0200

    feat: add initial home page markup

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,24 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
     <Link to="/page-2/">Go to page 2</Link>
+    <h1>Dale French</h1>
+    <h2>A Full Stack Developer who likes building stuff with</h2>
+    <h2>Styled Components</h2>
+
+    <footer>
+      <span>
+        © {new Date().getFullYear()} - Dale French
+        <br />
+        Built with <a href="#">Gatsby</a> • Hosted on <a href="#">Netlify</a> •
+        Code on <a href="#">GitHub</a>
+      </span>
+    </footer>
   </Layout>
 )
 
