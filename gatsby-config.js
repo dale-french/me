853ee1e2ad28044870c2e066d056f30fc286d837
Dale French <dalef@fmi.co.za>
2020-01-26 20:29:07 +0200

    feat: add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#FFFFFF`,
         theme_color: `#FFCC66`,
         display: `minimal-ui`,
-        icon: ``, // This path is relative to the root of the site.
+        icon: `src/images/me-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
