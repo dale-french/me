aec70f9eb2344b2f3175ff3e86e739550b10c7ef
Dale French <dalef@fmi.co.za>
2020-01-26 17:11:03 +0200

    chore: remove favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#FFFFFF`,
         theme_color: `#FFCC66`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: ``, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
