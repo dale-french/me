08ba0bf61b9ae2e1fd17790ce64a9e76f9d41085
Dale French <dalef@fmi.co.za>
2020-01-26 20:34:53 +0200

    feat: 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,43 @@
 import React from "react"
+import styled from "styled-components"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import theme from "../utils/theme"
+import { Layout, SEO, Sticky } from "../components"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="A (very) little bit about me" />
+    <Section>
+      <h1>
+        Page <span>Not</span> Found
+      </h1>
+      <h3>You just hit a route that {`doesn't`} exist 😐</h3>
+    </Section>
+    <Sticky />
   </Layout>
 )
 
 export default NotFoundPage
+
+// Component Styles
+const Section = styled.section`
+  text-align: center;
+  background: ${theme.colors.white};
+  max-width: 1100px;
+  margin: 0 auto;
+  padding: 60px 30px 30px;
+  h1 {
+    font-size: 8rem;
+    color: ${theme.colors.grey};
+    span {
+      color: ${theme.colors.orange};
+    }
+  }
+  h3 {
+    color: ${theme.colors.grey};
+    font-size: 2.5rem;
+    span {
+      color: ${theme.colors.orange};
+    }
+  }
+`
