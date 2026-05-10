7c846da40997f1e3b0c3aaa1817f556604dcd7e4
Dale French <hello@dalefrench.dev>
2026-05-10 16:20:54 +0200

    add tailwind 💨

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,91 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig, fontProviders } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig, fontProviders } from "astro/config";
 
-// https://astro.build/config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	fonts: [
-		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
-		},
-	],
+  site: "https://dalefrench.dev",
+  integrations: [mdx(), sitemap()],
+
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Neue Haas Grotesk Text",
+      cssVariable: "--font-neue-haas-grotesk-text",
+      fallbacks: ["sans-serif"],
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/neue-haas-grotesk-text--55--roman.woff2"],
+            weight: 400,
+            style: "normal",
+            display: "swap",
+          },
+          {
+            src: [
+              "./src/assets/fonts/neue-haas-grotesk-text--56--italic.woff2",
+            ],
+            weight: 400,
+            style: "italic",
+            display: "swap",
+          },
+          {
+            src: ["./src/assets/fonts/neue-haas-grotesk-text--75--bold.woff2"],
+            weight: 700,
+            style: "normal",
+            display: "swap",
+          },
+          {
+            src: [
+              "./src/assets/fonts/neue-haas-grotesk-text--76--bold-italic.woff2",
+            ],
+            weight: 700,
+            style: "italic",
+            display: "swap",
+          },
+        ],
+      },
+    },
+    {
+      provider: fontProviders.local(),
+      name: "Neue Haas Grotesk Display",
+      cssVariable: "--font-neue-haas-grotesk-display",
+      fallbacks: ["sans-serif"],
+      options: {
+        variants: [
+          {
+            src: [
+              "./src/assets/fonts/neue-haas-grotesk-display--65--medium.woff2",
+            ],
+            weight: 500,
+            style: "normal",
+            display: "swap",
+          },
+          {
+            src: [
+              "./src/assets/fonts/neue-haas-grotesk-display--66--medium-italic.woff2",
+            ],
+            weight: 500,
+            style: "italic",
+            display: "swap",
+          },
+          {
+            src: [
+              "./src/assets/fonts/neue-haas-grotesk-display--75--bold.woff2",
+            ],
+            weight: 700,
+            style: "normal",
+            display: "swap",
+          },
+        ],
+      },
+    },
+  ],
+
+  vite: {
+    plugins: [/** @type {any} */ (tailwindcss())],
+  },
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Dale French";
+export const SITE_DESCRIPTION = "Hi there. I'm Dale French.";
